b0a5717af3da41586d14cceb88725b51e7ced5cc
Deepak Subramanian <deepak.subu@gmail.com>
2014-03-02 20:43:10 -0800

    Ignored the target and the .idea directories

---
+++ src/main/java/sevenelevendoubles/PlayerDrinkTask.java
@@ -0,0 +1,11 @@
+package sevenelevendoubles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: deepak
+ * Date: 3/2/14
+ * Time: 5:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlayerDrinkTask {
+}
